9fd03dae331d5ce5f08a53e1c6c693d805e15b0d
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 17:13:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,11 @@ def prime?(x)
   if primes.include?(x) || x == 0 || x == 1 || x < 0
     return false
   
-  elsif 
+  else
+    for i in 2..(x + 1)
+      
+      return false
+    end
   end
   
   return true
