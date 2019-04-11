a0d56d7c6f8195ab40f0bebb629a94cbc98a4163
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 16:40:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(x)
   # Checks to see if it is an original prime
   primes = [2,3,5,7]
   
-  if primes.includes?(x) || x == 0 || x == 1 || x < 0
+  if primes.include?(x) || x == 0 || x == 1 || x < 0
     return false
   end
   
