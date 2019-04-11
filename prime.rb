603d37faac4cc1fdf587cfaa2ef41bf5d1972d05
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 16:13:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 # Add  code here!
-require 'Math'
 
 def prime?(x)
   # Checks to see if it is an original prime
   primes = [2,3,5,7]
-  if(math.sqrt(x).is_integer)
+  if(Math.sqrt(x).is_integer)
     return false
   end
 end
