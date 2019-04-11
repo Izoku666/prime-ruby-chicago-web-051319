5c340cfed5f4d1752af40b675002ea3886de7f42
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 16:09:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,17 +2,7 @@
 
 def prime?(x)
   # Checks to see if it is an original prime
-  if(x == 0 || x == 1)
-    return true
   primes = [2,3,5,7]
-  for(i in primes)
-    if(x == i)
-      return true
-  end
-  
-  for i in primes
-    if 2 * (n/2)
-  end
 end
 
 
