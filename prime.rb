3805ec2da657b6323dd9e292758dde9ae2a5b9ad
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 15:49:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,15 @@
 
 def prime?(x)
   primes = [2,3,5,7]
-    
+  for i in primes
+      if x % i == 0
+        return false
+      elsif x % i != 0
+        return prime?(x % i)
+      else
+        next
+      end
+      
   end
-  
-  
 end
+  
