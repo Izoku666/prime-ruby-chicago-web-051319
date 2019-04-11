3ecdad2aebd1d5782ed4f6dddeff37eeb59a72b9
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 16:29:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(x)
   # Checks to see if it is an original prime
   primes = [2,3,5,7]
-  if(primes.include?(x) == false || x == 1 || x == 0)
+  if primes.include?(x) == false || x == 1 || x == 0
       return true
   
   elsif(Math.sqrt(x).is_integer) # a prime number is always an int
