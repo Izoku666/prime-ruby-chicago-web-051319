3d089540edbaa1b2f3f439c3955af07f2987bdc5
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 16:38:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,10 @@ require 'Math'
 def prime?(x)
   # Checks to see if it is an original prime
   primes = [2,3,5,7]
-  if x < 0 return false
-    
-  elsif primes.include?(x) == false || x == 1 || x == 0
-      return true
-  
-  elsif Math.sqrt(x) is Integer
+ for d in 2..(x - 1)
+   if (x % d) == 0
     return false
+   end
   end
+  return true
 end
