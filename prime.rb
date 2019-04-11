1fd2fedf4771afa260cc1a38b025ceeb948d741a
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 15:51:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,15 @@
 # Add  code here!
 
 def prime?(x)
+  # Checks to see if it is an original prime
+  if(x == 0 || x == 1)
+    return true
   primes = [2,3,5,7]
+  for(i in primes)
+    if(x == i)
+      return true
+  end
+  
   for i in primes
       if x % i == 0
         return false
